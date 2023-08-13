666e428f4bb5f9a83d7304dec99f18f2f7120434
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2023-08-13 16:32:17 +0500

    remove unuserd imports from user.route.ts file

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,4 @@
-import { Request, Response, Router } from "express";
-import { z } from 'zod';
-import { client } from "../db/connection";
+import { Router } from "express";
 import { userController } from "../controllers/user.controller";
 
 const userRouter = Router();
