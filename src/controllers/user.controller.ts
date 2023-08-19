5587843bb6cb2d0f227c7ea4f76e372374de860c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2023-08-19 14:26:33 +0500

    updates

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,30 +1,28 @@
 import { Request, Response } from "express";
-import { z } from "zod";
+import jwt from 'jsonwebtoken';
 import { client } from "../db/connection";
+import { User, userSchema } from "../types/UserSchema";
 
 class UserController {
   async register(req: Request, res: Response) {
-    const userSchema = z.object({
-      email: z
-        .string({ required_error: 'email is a required field' })
-        .email({ message: 'invalid email address' }),
-      username: z.string({
-        required_error: 'username is a required field',
-        invalid_type_error: 'username must be a string',
-      }),
-      password: z.string({
-        required_error: 'password is a required field',
-        invalid_type_error: 'password must be a string',
-      }),
-    });
-
     try {
-      type User = z.infer<typeof userSchema>;
       const user = userSchema.parse(req.body);
-      const row = await client.query<User>(
+
+      const result = await client.query<User>(
         `INSERT INTO users (email, username, password)
-         VALUES ($1, $2, $3) RETURNING *`, [user.email, user.username, user.password]);
-      res.send(row.rows);
+         VALUES ($1, $2, $3) RETURNING *`, [user.email, user.username, user.password]
+      );
+
+      const dbUser = result.rows[0]
+      const token = jwt.sign(dbUser.id, process.env.JWT_SECRET || 'abcxyz');
+      const refreshToken = jwt.sign(dbUser.id, process.env.REFRESH_SECRET || 'xyzabc');
+
+      const tokens = await client.query(
+        `INSERT INTO tokens(token, refresh_token, user_id)
+        VALUES ($1, $2, $3, $4) RETURNING *`, [token, refreshToken, dbUser.id]
+      );
+
+      res.send({ dbUser, auth: tokens.rows[0] });
     } catch (e) {
       res.status(400).json(e)
     }
